73eacc3695fa177ee904f0b08496dc860d633189
natto <natto@natto-tp.(none)>
2012-12-31 13:43:36 -0500

    first version that retrieves pubmed/crossref with basic readme

---
+++ GetCitation/exampledata.py
@@ -0,0 +1,216 @@
+QueryResponse = {
+    'crossref.doi': '''\
+<?xml version="1.0" encoding="UTF-8"?>
+<doi_records>
+  <doi_record owner="10.1055" timestamp="2011-08-19 02:25:35">
+    <crossref>
+      <journal>
+        <journal_metadata language="de">
+          <full_title>Seminars in Neurology</full_title>
+          <abbrev_title>Semin Neurol</abbrev_title>
+          <issn media_type="print">0271-8235</issn>
+          <issn media_type="electronic">1098-9021</issn>
+        </journal_metadata>
+        <journal_issue>
+          <contributors>
+            <person_name sequence="first" contributor_role="editor">
+              <given_name>Karen L</given_name>
+              <surname>Roos</surname>
+            </person_name>
+            <person_name sequence="additional" contributor_role="editor">
+              <given_name>Alon Y</given_name>
+              <surname>Avidan</surname>
+            </person_name>
+          </contributors>
+          <publication_date media_type="print">
+            <month>03</month>
+            <year>2005</year>
+          </publication_date>
+          <journal_volume>
+            <volume>25</volume>
+          </journal_volume>
+          <issue>01</issue>
+        </journal_issue>
+        <journal_article publication_type="full_text" language="en">
+          <titles>
+            <title>Neurocognitive Consequences of Sleep Deprivation</title>
+          </titles>
+          <contributors>
+            <person_name sequence="first" contributor_role="author">
+              <given_name>Jeffrey S</given_name>
+              <surname>Durmer</surname>
+            </person_name>
+            <person_name sequence="additional" contributor_role="author">
+              <given_name>David F</given_name>
+              <surname>Dinges</surname>
+            </person_name>
+          </contributors>
+          <publication_date media_type="print">
+            <month>03</month>
+            <year>2005</year>
+          </publication_date>
+          <pages>
+            <first_page>117</first_page>
+            <last_page>129</last_page>
+          </pages>
+          <doi_data>
+            <doi>10.1055/s-2005-867080</doi>
+            <timestamp>2005032916592474</timestamp>
+            <resource>http://www.thieme-connect.de/DOI/DOI?10.1055/s-2005-867080</resource>
+          </doi_data>
+        </journal_article>
+      </journal>
+    </crossref>
+  </doi_record>
+</doi_records>
+''',
+    'pubmed.xml': '''\
+<?xml version="1.0"?>
+<!DOCTYPE PubmedArticleSet PUBLIC "-//NLM//DTD PubMedArticle, 1st January 2013//EN" "http://www.ncbi.nlm.nih.gov/corehtml/query/DTD/pubmed_130101.dtd">
+<PubmedArticleSet>
+<PubmedArticle>
+    <MedlineCitation Owner="NLM" Status="MEDLINE">
+        <PMID Version="1">15798944</PMID>
+        <DateCreated>
+            <Year>2005</Year>
+            <Month>03</Month>
+            <Day>30</Day>
+        </DateCreated>
+        <DateCompleted>
+            <Year>2005</Year>
+            <Month>05</Month>
+            <Day>24</Day>
+        </DateCompleted>
+        <DateRevised>
+            <Year>2007</Year>
+            <Month>11</Month>
+            <Day>14</Day>
+        </DateRevised>
+        <Article PubModel="Print">
+            <Journal>
+                <ISSN IssnType="Print">0271-8235</ISSN>
+                <JournalIssue CitedMedium="Print">
+                    <Volume>25</Volume>
+                    <Issue>1</Issue>
+                    <PubDate>
+                        <Year>2005</Year>
+                        <Month>Mar</Month>
+                    </PubDate>
+                </JournalIssue>
+                <Title>Seminars in neurology</Title>
+                <ISOAbbreviation>Semin Neurol</ISOAbbreviation>
+            </Journal>
+            <ArticleTitle>Neurocognitive consequences of sleep deprivation.</ArticleTitle>
+            <Pagination>
+                <MedlinePgn>117-29</MedlinePgn>
+            </Pagination>
+            <Abstract>
+                <AbstractText>Deficits in daytime performance due to sleep loss are experienced universally and associated with a significant social, financial, and human cost. Microsleeps, sleep attacks, and lapses in cognition increase with sleep loss as a function of state instability. Sleep deprivation studies repeatedly show a variable (negative) impact on mood, cognitive performance, and motor function due to an increasing sleep propensity and destabilization of the wake state. Specific neurocognitive domains including executive attention, working memory, and divergent higher cognitive functions are particularly vulnerable to sleep loss. In humans, functional metabolic and neurophysiological studies demonstrate that neural systems involved in executive function (i.e., prefrontal cortex) are more susceptible to sleep deprivation in some individuals than others. Recent chronic partial sleep deprivation experiments, which more closely replicate sleep loss in society, demonstrate that profound neurocognitive deficits accumulate over time in the face of subjective adaptation to the sensation of sleepiness. Sleep deprivation associated with disease-related sleep fragmentation (i.e., sleep apnea and restless legs syndrome) also results in neurocognitive performance decrements similar to those seen in sleep restriction studies. Performance deficits associated with sleep disorders are often viewed as a simple function of disease severity; however, recent experiments suggest that individual vulnerability to sleep loss may play a more critical role than previously thought.</AbstractText>
+            </Abstract>
+            <Affiliation>Program in Sleep Medicine, Department of Neurology, Emory University School of Medicine, Atlanta, GA 30322, USA.</Affiliation>
+            <AuthorList CompleteYN="Y">
+                <Author ValidYN="Y">
+                    <LastName>Durmer</LastName>
+                    <ForeName>Jeffrey S</ForeName>
+                    <Initials>JS</Initials>
+                </Author>
+                <Author ValidYN="Y">
+                    <LastName>Dinges</LastName>
+                    <ForeName>David F</ForeName>
+                    <Initials>DF</Initials>
+                </Author>
+            </AuthorList>
+            <Language>eng</Language>
+            <GrantList CompleteYN="Y">
+                <Grant>
+                    <GrantID>R01-NR04281</GrantID>
+                    <Acronym>NR</Acronym>
+                    <Agency>NINR NIH HHS</Agency>
+                    <Country>United States</Country>
+                </Grant>
+            </GrantList>
+            <PublicationTypeList>
+                <PublicationType>Journal Article</PublicationType>
+                <PublicationType>Research Support, Non-U.S. Gov't</PublicationType>
+                <PublicationType>Research Support, U.S. Gov't, Non-P.H.S.</PublicationType>
+                <PublicationType>Research Support, U.S. Gov't, P.H.S.</PublicationType>
+                <PublicationType>Review</PublicationType>
+            </PublicationTypeList>
+        </Article>
+        <MedlineJournalInfo>
+            <Country>United States</Country>
+            <MedlineTA>Semin Neurol</MedlineTA>
+            <NlmUniqueID>8111343</NlmUniqueID>
+            <ISSNLinking>0271-8235</ISSNLinking>
+        </MedlineJournalInfo>
+        <CitationSubset>IM</CitationSubset>
+        <CitationSubset>S</CitationSubset>
+        <MeshHeadingList>
+            <MeshHeading>
+                <DescriptorName MajorTopicYN="N">Cognition Disorders</DescriptorName>
+                <QualifierName MajorTopicYN="N">diagnosis</QualifierName>
+                <QualifierName MajorTopicYN="Y">etiology</QualifierName>
+                <QualifierName MajorTopicYN="N">physiopathology</QualifierName>
+            </MeshHeading>
+            <MeshHeading>
+                <DescriptorName MajorTopicYN="N">Humans</DescriptorName>
+            </MeshHeading>
+            <MeshHeading>
+                <DescriptorName MajorTopicYN="N">Sleep Deprivation</DescriptorName>
+                <QualifierName MajorTopicYN="Y">complications</QualifierName>
+                <QualifierName MajorTopicYN="N">diagnosis</QualifierName>
+                <QualifierName MajorTopicYN="N">physiopathology</QualifierName>
+            </MeshHeading>
+        </MeshHeadingList>
+        <NumberOfReferences>155</NumberOfReferences>
+        <KeywordList Owner="NASA">
+            <Keyword MajorTopicYN="N">NASA Discipline Space Human Factors</Keyword>
+            <Keyword MajorTopicYN="N">Non-NASA Center</Keyword>
+        </KeywordList>
+        <InvestigatorList>
+            <Investigator ValidYN="Y">
+                <LastName>Dinges</LastName>
+                <ForeName>D F</ForeName>
+                <Initials>DF</Initials>
+                <Affiliation>U PA Sch Med, Philadelphia</Affiliation>
+            </Investigator>
+        </InvestigatorList>
+        <GeneralNote Owner="NASA">Grant numbers: NASA NSBRI NCC 9-58.</GeneralNote>
+    </MedlineCitation>
+    <PubmedData>
+        <History>
+            <PubMedPubDate PubStatus="pubmed">
+                <Year>2005</Year>
+                <Month>3</Month>
+                <Day>31</Day>
+                <Hour>9</Hour>
+                <Minute>0</Minute>
+            </PubMedPubDate>
+            <PubMedPubDate PubStatus="medline">
+                <Year>2005</Year>
+                <Month>5</Month>
+                <Day>25</Day>
+                <Hour>9</Hour>
+                <Minute>0</Minute>
+            </PubMedPubDate>
+            <PubMedPubDate PubStatus="entrez">
+                <Year>2005</Year>
+                <Month>3</Month>
+                <Day>31</Day>
+                <Hour>9</Hour>
+                <Minute>0</Minute>
+            </PubMedPubDate>
+        </History>
+        <PublicationStatus>ppublish</PublicationStatus>
+        <ArticleIdList>
+            <ArticleId IdType="doi">10.1055/s-2005-867080</ArticleId>
+            <ArticleId IdType="pubmed">15798944</ArticleId>
+        </ArticleIdList>
+    </PubmedData>
+</PubmedArticle>
+
+</PubmedArticleSet>
+''',
+    }
+
+
